4472d57c3193d2141bcaadb0a06b73802a6322b9
sinagdev <ray.sinag.dev@gmail.com>
2023-11-05 20:22:04 -0700

    tabs.js: reached the goal functionality

--- src/scripts/sections/tabs.js
+++ src/scripts/sections/tabs.js
@@ -4,9 +4,9 @@ document.addEventListener('DOMContentLoaded', function () {
     dropdownContainers.forEach(function (container) {
         const dropdownButtons = container.querySelectorAll('[data-toggle="custom-tab"]')
         const dropdownToggle = container.querySelector('.btn.dropdown-toggle')
-        const tabPanes = container.parentElement.querySelectorAll('.tab-pane')
+        const tabContent = container.parentElement.querySelector('.tab-content')
 
-        // Set the initial content of the dropdown button to match the first tab
+        // Set the initial content of the dropdown button to match the first tab item
         const firstDropdownButton = dropdownButtons[0]
         dropdownToggle.innerText = firstDropdownButton.innerText
 
@@ -18,14 +18,15 @@ document.addEventListener('DOMContentLoaded', function () {
                 // Update the dropdown-toggle button text
                 dropdownToggle.innerText = selectedOptionText
 
-                // Hide all tab panes
+                // Show the selected tab pane and hide others
+                const tabPanes = tabContent.querySelectorAll('.tab-pane')
                 tabPanes.forEach(function (pane) {
-                    pane.classList.remove('show', 'active')
+                    if (pane.id === targetId.substring(1)) {
+                        pane.classList.add('show', 'active')
+                    } else {
+                        pane.classList.remove('show', 'active')
+                    }
                 })
-
-                // Show the selected tab pane
-                const selectedTabContent = document.querySelector(targetId)
-                selectedTabContent.classList.add('show', 'active')
             })
         })
     })
